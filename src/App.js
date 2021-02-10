143d9f4f6c9488f43514a543318104bdea6de721
Precious chiedu <udegbue69@gmail.com>
2021-02-10 07:13:19 +0100

    created layout

---
+++ src/App.js
@@ -0,0 +1,45 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="smallDiv">
+        <div className="innerDiv">
+          <div>
+            <p>image</p>
+            <p>dashboard</p>
+          </div>
+          <div>
+            <p>image</p>
+            <p>dashboard</p>
+          </div>
+          <div>
+            <p>image</p>
+            <p>dashboard</p>
+          </div>
+          <div>
+            <p>image</p>
+            <p>dashboard</p>
+          </div>
+        </div>
+        <div className='settings'>
+        <div className='settingsDiv'>
+            <p>image</p>
+            <p>settings</p>
+          </div>
+        </div>
+      </div>
+      <div className='bigDiv'>
+        <div className='searchBar'>
+          <div className='searchCon'>
+            <div className='searchBox'>
+              <div>icon</div>
+              <div>search</div>
+            </div>
+          </div>
+          <div className='notification'>image and notification</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
