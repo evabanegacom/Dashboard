f343ef8553c2832aba4c279812ccc1fd72f98f77
Precious chiedu <udegbue69@gmail.com>
2021-02-10 11:04:09 +0100

    added numbers

--- src/coloredBoxes.js
+++ src/coloredBoxes.js
@@ -6,27 +6,27 @@ const ColoredBoxes = () => {
         <div className='boxes'>
           <div className='smallbox box1'>
               <p>Registered companies</p>
-              <p>849</p>
-              <p>1.33%</p>
-              <p>something sha</p>
+              <p className='number'>849</p>
+              <p className='percent'>&#8593; 1.33%</p>
+              <p className='lastweek'>since last week</p>
           </div>
           <div className='smallbox box2'>
               <p>Registered companies</p>
-              <p>849</p>
-              <p>1.33%</p>
-              <p>something sha</p>
+              <p className='number'>849</p>
+              <p className='percent1'>&#8595; 2.43%</p>
+              <p className='lastweek'>since last week</p>
           </div>
           <div className='smallbox box3'>
               <p>Registered companies</p>
-              <p>849</p>
-              <p>1.33%</p>
-              <p>something sha</p>
+              <p className='number'>849</p>
+              <p className='percent'>1.33%</p>
+              <p className='lastweek'>since last week</p>
           </div>
           <div className='smallbox box4'>
               <p>Registered companies</p>
-              <p>849</p>
-              <p>1.33%</p>
-              <p>something sha</p>
+              <p className='number'>849</p>
+              <p className='percent1'>3.45%</p>
+              <p className='lastweek'>since last week</p>
           </div>
         </div>
     )
